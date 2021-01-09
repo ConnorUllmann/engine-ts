c19b44f1cc19b3f6ed2372afba46743208ea6f19
Connor Ullmann <c.ullmann@criteo.com>
2021-01-09 02:01:51 -0500

    add file save function

--- core/utils.ts
+++ core/utils.ts
@@ -46,6 +46,14 @@ export async function sleep(milliseconds: number) {
     await new Promise(resolve => setTimeout(resolve, milliseconds));
 }
 
+export function saveFile(text: string, fileName: string, contentType: string='text/plain') {
+    var a = document.createElement("a");
+    var file = new Blob([text], {type: contentType});
+    a.href = URL.createObjectURL(file);
+    a.download = fileName;
+    a.click();
+}
+
 export const tau: number = Math.PI * 2;
 
 // TODO: remove in favor of Geometry.distanceSq/distance
