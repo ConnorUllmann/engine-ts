8df5e6f3fc3f67074ca05ac4eb683a6b761f1f1c
Connor Ullmann <c.ullmann@hotmail.com>
2021-04-25 16:17:31 -0400

    add function to create an image element that is a snapshot of a canvas

--- visuals/create-canvas.ts
+++ visuals/create-canvas.ts
@@ -4,3 +4,14 @@ export function createCanvas(width: number, height: number): HTMLCanvasElement {
     canvas.height = height;
     return canvas;
 };
+
+export function createCanvasScreenshotElement(canvas: HTMLCanvasElement): HTMLImageElement {
+    var newImg = document.createElement('img');
+    canvas.toBlob(function(blob) {
+        var url = URL.createObjectURL(blob);
+        newImg.onload = function() { URL.revokeObjectURL(url); };
+        newImg.src = url;
+        newImg.style.padding="2px";
+    });
+    return newImg;
+}
