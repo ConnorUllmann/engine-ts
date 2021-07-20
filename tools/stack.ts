379e114bf86ebefa18a70bcfe4196f018a30afb6
Connor Ullmann <c.ullmann@hotmail.com>
2021-07-19 20:32:51 -0400

    add stack functions and add function to escape regex

--- tools/stack.ts
+++ tools/stack.ts
@@ -1,9 +1,10 @@
 export class Stack<T> {
-    private readonly elements: T[] = [];
+    public readonly elements: T[] = [];
     constructor() {}
 
     public get length(): number { return this.elements.length; }
     public isEmpty(): boolean { return this.elements.length <= 0; }
-    public pop(): T | null { return this.elements.pop() ?? null; };
-    public push(item: T): void { this.elements.push(item); };
+    public pop(): T | null { return this.elements.pop() ?? null; }
+    public push(item: T): void { this.elements.push(item); }
+    public get top(): T | null { return this.elements.last(); }
 }
