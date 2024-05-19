9b521ee220fa95b02933579504420d84a14e3318
Connor Ullmann <c.ullmann@hotmail.com>
2024-05-19 02:10:23 -0400

    add ability to save array buffers in saveFile method

--- tools/save-file.ts
+++ tools/save-file.ts
@@ -1,4 +1,4 @@
-export function saveFile(text: string, fileName: string, contentType: string = 'text/plain') {
+export function saveFile(text: string | ArrayBufferLike, fileName: string, contentType: string = 'text/plain') {
   var a = document.createElement('a');
   var file = new Blob([text], { type: contentType });
   a.href = URL.createObjectURL(file);
